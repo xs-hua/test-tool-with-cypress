a4ed5508a0a934fc7d8014a88f9f8de157b61dda
xinshu hua <work.hua.xinshu@gmail.com>
2021-12-14 18:36:23 +0100

    add custom cypress command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,3 +25,78 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 
+
+
+
+Cypress.Commands.add('createOneCustomer', (axios,config) => {
+
+    let uuid;
+    //set up beta environment
+    let postUrl = config.url.replace("beta",config.env);
+    let putUrl=postUrl;
+    let postUrlFinal = config.postUrlFinal.replace("beta", config.env)
+    let postData={"payload":{}};//as body 
+    let putData=config.putData;
+    let postDataFinal=config.postDataFinal;
+    let headers = config.headers;
+    
+    async function getPostResponse(){
+
+        let postResult = await
+        axios({
+            method: "POST",
+            url: postUrl,
+            data: postData,
+            headers: headers 
+        })
+
+        return postResult;
+    }
+
+    async function getPutResponse(){
+
+        let response = await getPostResponse();
+        console.log(response);
+        uuid=response.data.lead.uuid;
+        putUrl=postUrl+"/"+uuid;
+        putData.uuid=uuid;
+        
+
+        let putResult = await
+        axios({
+            method: "PUT",
+            url: putUrl,
+            data: putData,
+            headers: headers
+        });
+
+        return putResult;
+    }
+
+    async function getPostResponseFinal(){
+
+        let response = await getPutResponse();
+        console.log(response);
+        postDataFinal.uuid=response.data.lead.uuid;
+
+        let postResult = await
+        axios({
+            method: "POST",
+            url: postUrlFinal,
+            data: postDataFinal,
+            headers: headers
+        });
+
+        console.log(postResult);
+
+        return postResult;
+    }
+
+    getPostResponseFinal();
+
+})
+
+
+
+
+
